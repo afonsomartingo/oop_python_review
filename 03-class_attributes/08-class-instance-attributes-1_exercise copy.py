265d453e378a78a19b03c53ebd07db502384e49e
Afonso Carvalho <afonso.martingo.c@gmail.com>
2024-11-07 10:57:22 +0000

    Class attributes exercise

---
+++ 03-class_attributes/08-class-instance-attributes-1_exercise copy.py
@@ -0,0 +1,18 @@
+'''Problem: Object Counter Class
+Description:
+
+You are tasked with creating a class called ObjectCounter that helps keep track of the number of instances created from the class. Each instance should have a unique ID assigned to it, which is an incrementing number starting from 1. Additionally, each instance will have an info attribute containing a string, which can be set at the time of instantiation.
+
+The class should have the following methods:
+
+__init__(self, info):
+
+Initializes the instance with a unique ID and assigns the info attribute.
+Updates the class attribute count each time a new instance is created.
+get_instance_info(self):
+
+Prints the instance's unique ID and its info attribute.
+get_total_instances() (class method):
+
+Prints the total number of instances created so far.'''
+
